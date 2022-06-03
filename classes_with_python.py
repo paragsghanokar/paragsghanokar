0e4b8566f6d9a77471c0fa275a086ff2df697808
paragsghanokar <103407378+paragsghanokar@users.noreply.github.com>
2022-06-03 18:15:53 +0530

    Update classes_with_python.py

--- classes_with_python.py
+++ classes_with_python.py
@@ -13,27 +13,27 @@ class CSVRead:
 class MySqlExecution:
   
     def create_connection(self):
-        MY_DB = mysql.connector.connect(
+        LOCAL_DB = mysql.connector.connect(
         host="localhost",
         user="root",
         password="Cmile@123",
         database="emaildb"
         )
-        return  MY_DB
+        return  LOCAL_DB
         
     def write_csv_to_sql(self, csv_data):    
-        myCursor=con.cursor()
+        my_cursor=con.cursor()
         for row in csv_data:
             print(row)
             query_1 = "INSERT INTO emailid(Username, Loginemail, Identifier, Firstname, Lastname) VALUES (%s, %s, %s, %s, %s)"
-            myCursor.execute(query_1,row)
+            my_cursor.execute(query_1,row)
             con.commit()
         return myCursor
         
-    def fetch_data_from_table(self, myCursor):
+    def fetch_data_from_table(self, my_cursor):
         query_2=("SELECT * FROM emailid")       
-        myCursor.execute(query_2)
-        result = myCursor.fetchall()
+        my_cursor.execute(query_2)
+        result = my_cursor.fetchall()
         print('Inserted records:\n')
         for info in result:
             print(info)
@@ -46,7 +46,7 @@ dbConnect= MySqlExecution()
 rCSV=CSVRead()
 con=dbConnect.create_connection()
 csv_data=rCSV.import_csv()
-myCursor=dbConnect.write_csv_to_sql(csv_data)
-dbConnect.fetch_data_from_table(myCursor)
+my_cursor=dbConnect.write_csv_to_sql(csv_data)
+dbConnect.fetch_data_from_table(my_cursor)
 dbConnect.close_connection()    
 
